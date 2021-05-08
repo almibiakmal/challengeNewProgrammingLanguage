ef936946eb5846d7ffaf88d4e4b95e659bd6ec8a
almibiakmal <ardiprayuda11@gmail.com>
2021-05-09 01:14:31 +0700

    Add challenge 4 in javascript

---
+++ javascript/challenge_4.js
@@ -0,0 +1,32 @@
+let lebar = 6;
+let limit = lebar;
+let awal = false;
+
+while (true){
+    let temp = ""; 
+	for(let i = 1; i <= lebar; i++){
+		temp += i+" ";
+    }
+    console.log(temp);	
+
+	if(!awal){
+		lebar--;
+    }
+
+	if(lebar === 0){
+		awal = true;
+    }
+
+	if(awal){
+		if(lebar === 0){
+			lebar += 2;
+        }else{
+			lebar++;
+        }
+    }
+
+	if(lebar > limit){
+		console.log("Selesai");
+		break
+    }
+}

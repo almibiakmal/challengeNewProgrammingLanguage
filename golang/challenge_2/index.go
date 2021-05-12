959b95a657724bab762972c1db909c46558a6b83
almibiakmal <ardiprayuda11@gmail.com>
2021-05-13 01:54:18 +0700

    Update challenge_2 with add input user

--- golang/challenge_2/index.go
+++ golang/challenge_2/index.go
@@ -1,16 +1,21 @@
 package main
-import
-	"fmt"
+import "fmt"
 
 func main(){
-	high := 6
-	wide := 6
+	var high int
+	var wide int
+
+	fmt.Print("Tinggi : ")
+    fmt.Scanf("%d", &high)
+
+	fmt.Print("Lebar : ")
+    fmt.Scanf("%d", &wide)
 
 	for high > 0{
 		for i := 1; i <= wide; i++{
 			fmt.Print(i, " ")
 		}
-		
+
 		fmt.Println("")
 		high--
 	}

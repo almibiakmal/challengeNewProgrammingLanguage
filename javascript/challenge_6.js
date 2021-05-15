9ee3d0b0e91490e4f8e9fbe29c5e0d2ca912caa9
almibiakmal <ardiprayuda11@gmail.com>
2021-05-15 23:51:52 +0700

    Update code challenge 6

--- javascript/challenge_6.js
+++ javascript/challenge_6.js
@@ -2,12 +2,51 @@ const readlineSync = require('readline-sync')
 
 console.log("====================================\nWelcome to Sorting Program\n====================================\n");
 
-let angka = [];
-try{
-	while(true){
-        let temp = readlineSync.questionInt("Enter a number : ");
-		angka.push(temp)
+let angkas = [];
+
+if(angkas.length > 1){
+    while(true){
+        let answare = readlineSync.question("Do you want to start sorting (y/n) ? ");
+        
+        switch(answare){
+            case "y":
+                let tempAngka = "";
+                angkas.forEach((angka)=>{tempAngka += ", "+angka});
+                console.log(`Input : ${tempAngka}`);
+                console.log("Processing...");
+
+                while(true){
+                    let move = 0;
+
+                    angkas.forEach((val, index)=>{
+                        if(){
+                            
+                        }
+                    });
+
+                    if(move === 0){
+                        break;
+                    }
+                }
+
+                console.log("Success...");
+                console.log(`Output : ${angkas}`);
+
+                break;
+            case "n":
+                break;
+            default:
+                console.log("Unknow, please try again...");
+        }
+        
+    }   
+}else{
+    if(angkas.length === 1){
+        console.log(`Input: ${angkas[0]}`);
+        console.log(`Output: ${angkas[0]}`);
+    }else{
+        console.log("Input is empty");
     }
-}catch(ValueError){
-    console.log("Process input stoped.");
 }
+
+console.log("Finish");
